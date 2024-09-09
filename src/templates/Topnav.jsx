af00cf40f1f26fe8735ea892922dd543090795b2
unknown <abdealidahodwala163@gmail.com>
2024-09-09 20:53:34 +0530

    Integrate Topnav and header api

--- src/templates/Topnav.jsx
+++ src/templates/Topnav.jsx
@@ -1,12 +1,30 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import axios from "../utils/axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import noimage from "../assets/noimage.jpg"
 
 function Topnav() {
-    const [query,setquery] = useState("");
-    
-    
+  const [query, setquery] = useState("");
+  const [search, setSearch] = useState([]);
+
+  async function getSearches() {
+    try {
+      const { data } = await axios.get(`/search/multi?query=${query}`);
+      // console.log(data.results);
+
+      setSearch(data.results);
+    } catch (error) {
+      console.log("Error = ", error);
+    }
+  }
+
+  useEffect(() => {
+    getSearches();
+  }, [query]);
+
   return (
     <div className="w-[80%] h-full ">
       <div className="navbar pt-3 relative justify-start ml-[30%] flex gap-5 text-zinc-300 ">
@@ -17,17 +35,28 @@ function Topnav() {
           name="search"
           id="search"
           placeholder="Search anything"
-          onChange={(e)=>setquery(e.target.value)}
+          onChange={(e) => setquery(e.target.value)}
           value={query}
         />
 
         {/* query box */}
         <div className="absolute w-[80%] mt-1 max-h-[50vh] left-[8%] bg-zinc-300 top-[90%] rounded-md overflow-auto">
-          {/* <Link className="hover:text-black hover:bg-zinc-300 duration-300 font-semibold text-zinc-600 p-7  border-b-[1px] flex justify-start items-center">
-            <img src="" alt="" />
-            <span>Hello world</span>
-          </Link> */}
-          
+          {search.map((item, index) => {
+            return (
+              <Link
+                key={index}
+                className="hover:text-black hover:bg-zinc-300 duration-300 font-semibold text-zinc-600 p-7  border-b-[1px] flex justify-start items-center"
+              >
+                <img className="w-[10vh] h-[10vh] rounded-lg object-cover mr-5" src={item.backdrop_path || item.profile_path ? `https://image.tmdb.org/t/p/original${item.backdrop_path || item.profile_path} ` : noimage } alt="" />
+                <span>
+                  {item.name ||
+                    item.title ||
+                    item.original_name ||
+                    item.original_title}
+                </span>
+              </Link>
+            );
+          })}
         </div>
       </div>
     </div>
