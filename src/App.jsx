55884821c156f671d08fa66ff31861b6f82e6f26
unknown <abdealidahodwala163@gmail.com>
2024-09-11 20:46:46 +0530

    Movies tab created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import Trending from './components/Trending'
 import Popular from './components/Popular'
+import Movie from './components/Movie'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home />}/>
         <Route path='/trending' element={<Trending />}/>
         <Route path='/popular' element={<Popular />}/>
+        <Route path='/movies' element={<Movie />}/>
       </Routes>
     </div>
   )
--- src/templates/Sidenav.jsx
+++ src/templates/Sidenav.jsx
@@ -26,7 +26,7 @@ function Sidenav() {
             <i className="mr-3 ri-bard-fill"></i>
             <h1>Popular</h1>
           </Link>
-          <Link className="flex text-xl rounded-lg duration-300 p-3 hover:bg-[#6556cd] hover:text-white">
+          <Link to={"/movies"} className="flex text-xl rounded-lg duration-300 p-3 hover:bg-[#6556cd] hover:text-white">
             <i className="mr-3 ri-movie-fill"></i>
             <h1>Movies</h1>
           </Link>
